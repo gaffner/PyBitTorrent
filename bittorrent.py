b70f48177f28839ce0a7f32abc9638048bb9a15a
john57 <your_email_address@example.com>
2022-10-20 22:26:19 +0300

    Added argument and bittorrent encoding parse

--- bittorrent.py
+++ bittorrent.py
@@ -1,12 +1,20 @@
+from typing import Dict
+
+from bcoding import bdecode
+
 from peers_manager import PeersManager
 from tracker_manager import TrackerManager
 
 
 class BitTorrentClient:
-    def __init__(self, torrent_file_path):
-        self.torrent_file = open(torrent_file_path, 'rb')
-        self.peer_manager: PeersManager = None
-        self.tracker_manager: TrackerManager = None
+    def __init__(self, torrent):
+        self.config: Dict = {}
+        self.peer_manager: PeersManager = PeersManager()
+        self.tracker_manager: TrackerManager = TrackerManager()
+
+        # Decode the config file and assign it
+        torrent_data = torrent.read()
+        self.config = bdecode(torrent_data)
 
     def start(self):
         pass
--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+import logging
+import sys
+from argparse import ArgumentParser, FileType
+
+from bittorrent import BitTorrentClient
+
+
 def main():
-    print('Test')
+    """
+    Script for downloading torrent files
+    """
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S'
+                        )
+
+    parser = ArgumentParser(main.__doc__)
+    parser.add_argument('--torrent', nargs=1, type=FileType('rb'), help='Path of the Torrent file')
+    args = parser.parse_args()
+    print(args)
+
+    torrentClient = BitTorrentClient(args.torrent[0])
+    torrentClient.start()
 
 
 if __name__ == '__main__':
